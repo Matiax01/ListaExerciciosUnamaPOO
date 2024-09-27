451bd0984ca19e0893cba6912ff5f72271eebeb7
AlesSandro Silva <101981656+AlessandroDaCostaESilva@users.noreply.github.com>
2024-09-27 03:09:28 +0000

    criacao da classe Fatura

--- lojaDSuprimentos/Fatura.java
+++ lojaDSuprimentos/Fatura.java
@@ -1,5 +1,47 @@
 package lojaDSuprimentos;
 
 public class Fatura{
+    private String codigoProduto;
+    private String descricaoProduto;
+    private int quantidadeComprada;
+    private double precoPorItem;
+
+    public Fatura(String codigoProduto, String descricaoProduto, int quantidadeComprada, double precoPorItem){
+        this.codigoProduto = codigoProduto;
+        this.descricaoProduto = descricaoProduto;
+        this.quantidadeComprada = quantidadeComprada;
+        this.precoPorItem = precoPorItem;
+    }
+
+    public String getCodigoProduto(){
+        return this.codigoProduto;
+    }
+
+    public void setCodigoProduto(String codigoProduto){
+        this.codigoProduto = codigoProduto;
+    }
     
+    public String getDescricaoProduto(){
+        return this.descricaoProduto;
+    }
+
+    public void setDescricaoProduto(String descricaoProduto){
+        this.descricaoProduto = descricaoProduto;
+    }
+
+    public int getQuantidadeComprada(){
+        return this.quantidadeComprada
+    }
+
+    public void setQuantidadeComprada(int quantidadeComprada){
+        this.quantidadeComprada = quantidadeComprada;
+    }
+
+    public double getPrecoPorItem(){
+        return this.precoPorItem
+    }
+
+    public void setPrecoPorItem(double precoPorItem){
+        this.precoPorItem = precoPorItem;
+    }
 }
